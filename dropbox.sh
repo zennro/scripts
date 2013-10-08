63780afd54bb6d94cf51914cd00110c1b2bf01ab
Skipper Seabold <jsseabold@gmail.com>
2013-10-08 12:06:04 +0000

    Fix syntax error.

--- dropbox.sh
+++ dropbox.sh
@@ -2,4 +2,4 @@
 
 # allow some setup time
 sleep 10 
-((ping -w5 -c3 8.8.8.8 || ping -w5 -c3 4.2.2.1) > /dev/null 2>&1) && /home/skipper/.dropbox-dist/dropboxd & || (exit 1)
+((ping -w5 -c3 8.8.8.8 || ping -w5 -c3 4.2.2.1) > /dev/null 2>&1) && (/home/skipper/.dropbox-dist/dropboxd &) || (exit 1)
