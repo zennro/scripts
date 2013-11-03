aefa277194a8c3a39ad06970890ace0ed215e1b9
Skipper Seabold <jsseabold@gmail.com>
2013-11-03 19:28:57 +0000

    Add comment

--- dropbox.sh
+++ dropbox.sh
@@ -2,4 +2,5 @@
 
 # allow some setup time
 sleep 10 
+# run dropbox only if you have a working internet connection
 ((ping -w5 -c3 8.8.8.8 || ping -w5 -c3 4.2.2.1) > /dev/null 2>&1) && (/home/skipper/.dropbox-dist/dropboxd &) || (exit 1)
