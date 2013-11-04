7cd52812e0da68d0ef84460a335510fcadca180e
Skipper Seabold <jsseabold@gmail.com>
2013-11-04 09:19:11 +0000

    Fix path

--- dropbox.sh
+++ dropbox.sh
@@ -3,4 +3,4 @@
 # allow some setup time
 sleep 10 
 # run dropbox only if you have a working internet connection
-((ping -w5 -c3 8.8.8.8 || ping -w5 -c3 4.2.2.1) > /dev/null 2>&1) && (/home/skipper/.dropbox-dist/dropboxd &) || (exit 1)
+((ping -w5 -c3 8.8.8.8 || ping -w5 -c3 4.2.2.1) > /dev/null 2>&1) && (/home/skipper/src/.dropbox-dist/dropboxd &) || (exit 1)
