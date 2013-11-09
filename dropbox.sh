3f713eb8d311bb02bcb60d6a170787ea8505cfb9
Skipper Seabold <jsseabold@gmail.com>
2013-11-09 14:01:05 +0000

    Remove old autostart scripts

--- dropbox.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-# allow some setup time
-sleep 10 
-# run dropbox only if you have a working internet connection
-((ping -w5 -c3 8.8.8.8 || ping -w5 -c3 4.2.2.1) > /dev/null 2>&1) && (/home/skipper/src/.dropbox-dist/dropboxd &) || (exit 1)
