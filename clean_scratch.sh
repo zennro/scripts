2822d687b5dec75c800ffbcf8d0bfe90a445aad1
Skipper Seabold <jsseabold@gmail.com>
2014-01-22 11:33:46 -0500

    Fix display and environment

--- clean_scratch.sh
+++ clean_scratch.sh
@@ -1,5 +1,7 @@
-#! /usr/bin
+#!/bin/bash
 
+export DISPLAY=:0
+export XAUTHORITY=/home/skipper/.Xauthority
 kdialog --passivepopup "The ~/scratch/ directory will be emptied in 1 minute." 5
 sleep 60
 rm -rf /home/skipper/scratch/* 
