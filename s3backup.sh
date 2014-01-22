a6727b6ed080b33df22b422d15f14a5e6e01735f
Skipper Seabold <jsseabold@gmail.com>
2014-01-22 11:34:00 -0500

    Fix display for notification

--- s3backup.sh
+++ s3backup.sh
@@ -7,6 +7,8 @@ else
     /usr/bin/s3fs skipper-lenovo-laptop-backup /mnt/backup/s3 -ouse_cache=/tmp
     BACKUP_DIR=/mnt/backup/s3/
 fi
+export DISPLAY=:0
+export XAUTHORITY=/home/skipper/.Xauthority
 kdialog --passivepopup "Your S3 backup job has started" 5
 /usr/bin/rsync -avrz --delete --inplace --stats --partial --log-file=log.file --exclude-from=/home/skipper/src/scripts/exclude --files-from=/home/skipper/src/scripts/backup.files /home/skipper/ $BACKUP_DIR
 mv log.file backup.log.`date +"%Y%m%d%H%M%S"`
